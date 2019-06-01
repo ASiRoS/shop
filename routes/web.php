a59a10af9785326c693f794624e753e0e82e99ef
Artyk Chonbashev <takahiroasiro@gmail.com>
2019-06-01 13:57:27 +0600

    Test mail

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Mail\SendAnswerMail;
+
 Route::get('/', 'HomeController')->name('home');
 
 Route::post('/payment/{product}/choose', 'PaymentController@choose')->name('payment.choose');
 Route::get('/payment/success', 'PaymentController@success')->name('payment.success');
 
 Route::get('/product/{product}', 'ProductController')->name('product');
+
+Route::get('/test', function() {
+    \Illuminate\Support\Facades\Mail::to('takahiroasiro@gmail.com')->send(new \App\Mail\Test());
+});
